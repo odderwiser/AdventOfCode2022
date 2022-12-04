ac6dc01165b72c215367eaee135bffc202f0de4c
Aleksandra Wolska <wcqaguxa@gmail.com>
2022-12-04 12:51:34 +0100

    day 5 setup

---
+++ src/day05/mod.rs
@@ -0,0 +1,39 @@
+use itertools::Itertools;
+
+fn part_1(input: &str) -> usize {
+    0
+}
+
+fn part_2(input: &str) -> usize {
+    0
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::day05::{part_1, part_2};
+
+    #[test]
+    fn part_1_example() {
+        assert_eq!(0, part_1(include_str!("test1.txt")));
+    }
+
+    #[test]
+    fn part_1_puzzle() {
+        let output = part_1(include_str!("input.txt"));
+        print!("{}", output);
+        //assert_eq!(547, output);
+    }
+
+    #[test]
+    fn part_2_example2() {
+        assert_eq!(0, part_2(include_str!("test1.txt")));
+    }
+
+    #[test]
+    fn part_2_puzzle() {
+        let output = part_2(include_str!("input.txt"));
+        print!("{}", output);
+        //assert_eq!(, output);
+    }
+}
