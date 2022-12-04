7496a28b5b5e73c9305ec266bd719ab7d38df9cb
Aleksandra Wolska <wcqaguxa@gmail.com>
2022-12-04 07:40:17 +0100

    day 4

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -1,19 +1,28 @@
 use itertools::Itertools;
 
-fn parse_input(filename: &str) -> impl Iterator<Item = &str> {
-    filename.lines()
-}
-
-fn part_2(input: &str) ->  usize {
-    parse_input(input);
-    0
+fn part_2(input: &str) -> usize {
+    count(input, |a, b| a || b)
 }
 
 fn part_1(input: &str) -> usize {
-    parse_input(input);
-    0
+    count(input, |a, b| a && b)
 }
 
+fn count(input: &str, logic: impl Fn(bool, bool) -> bool) -> usize {
+    input
+        .lines()
+        .flat_map(|x| {
+            x.split([',', '-'])
+                .map(|y| y.parse::<usize>().unwrap())
+                .tuples()
+        })
+        .tuples()
+        .filter(|((a, b), (c, d))| {
+            logic((a..=b).contains(&c), (a..=b).contains(&d))
+                || logic((c..=d).contains(&a), (c..=d).contains(&b))
+        })
+        .count()
+}
 
 #[cfg(test)]
 mod test {
@@ -21,25 +30,25 @@ mod test {
 
     #[test]
     fn part_1_example() {
-        assert_eq!(0, part_1(include_str!("test1.txt")));
+        assert_eq!(2, part_1(include_str!("test1.txt")));
     }
 
     #[test]
     fn part_1_puzzle() {
         let output = part_1(include_str!("input.txt"));
-        print!("{}", output);
-        //assert_eq!(0, output);
+        //print!("{}", output);
+        assert_eq!(547, output);
     }
 
     #[test]
     fn part_2_example2() {
-        assert_eq!(0, part_1(include_str!("test1.txt")));
+        assert_eq!(4, part_2(include_str!("test1.txt")));
     }
 
     #[test]
     fn part_2_puzzle() {
         let output = part_2(include_str!("input.txt"));
-        print!("{}", output);
-        //assert_eq!(0, output);
+        //print!("{}", output);
+        assert_eq!(843, output);
     }
 }
